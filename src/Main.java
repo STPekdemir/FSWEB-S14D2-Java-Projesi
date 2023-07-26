520874a7672e0fee179f8d14a69286cea5e393d9
STPekdemir <119021180+STPekdemir@users.noreply.github.com>
2023-07-26 19:44:22 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import com.Workintech.Sprint_14.Bed;
+import com.Workintech.Sprint_14.Bedroom;
+import com.Workintech.Sprint_14.Ceiling;
+import com.Workintech.Sprint_14.Lamp;
+import com.Workintech.Sprint_14.Wall;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Wall wall1 = new Wall("West");
+
+        Wall wall2 = new Wall("East");
+
+        Wall wall3 = new Wall("South");
+
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedRoom = new Bedroom("YOUR NAME HERE", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+
+        bedRoom.makeBed();
+
+        bedRoom.getLamp().turnOn();
+
+        /*
+         * Output
+         * Bed is being made
+         * The bed is being made.
+         * Lamp is being turned on.
+         */
     }
 }
---
+++ src/com/Workintech/Sprint_14/Ceiling.java
@@ -0,0 +1,21 @@
+package com.Workintech.Sprint_14;
+
+public class Ceiling {
+    private int height;
+    // painted color niye int?
+
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
